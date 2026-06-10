55cff4d1afcbb2da7ef888069c971e5bacf39f27
Milan Rai <milanraii014@gmail.com>
2026-06-11 04:43:32 +0545

    push

--- DomesticCylinder.java
+++ DomesticCylinder.java
@@ -1,6 +1,7 @@
 /**
- * Child class of LPG Cylinder that represent a domestic cylinder for the household usage.
- * This class haldles subsidy-related calculations and store 
+ * Child class of LPG Cylinder that represent a domestic cylinder for the
+ * household usage.
+ * This class haldles subsidy-related calculations and store
  * the cizizenship information requires for domestic cylinder registration.
  * 
  * @author Milan Rai
@@ -9,34 +10,37 @@
 public class DomesticCylinder extends LPGCylinder {
     private double subsidyAmount;
     private String citizenshipNumber;
-    private int quantityOfOrderCylinder; //Number of cylinders ordered.
+    private int quantityOfOrderCylinder; // Number of cylinders ordered.
     private int monthlyUsedCylinder = 0;
 
     /**
      * Initialize a domestic cylinder with LPG Cylinder and customer details.
      * 
-     * @param cylinderId the unique identifier of the cylinder
-     * @param cylinderType the type of the cylinder
-     * @param bookingId the booking ID assigned to the cylinder
-     * @param basePrice the base price of the domestic cylinder
-     * @param weight the weight of the domestic cylinder in kilograms
-     * @param subsidyAmount the subsidy amount provided for the customer
-     * @param citizenshipNumber the customer's citizenship number
+     * @param cylinderId              the unique identifier of the cylinder
+     * @param cylinderType            the type of the cylinder
+     * @param bookingId               the booking ID assigned to the cylinder
+     * @param basePrice               the base price of the domestic cylinder
+     * @param weight                  the weight of the domestic cylinder in
+     *                                kilograms
+     * @param subsidyAmount           the subsidy amount provided for the customer
+     * @param citizenshipNumber       the customer's citizenship number
      * @param quantityOfOrderCylinder the number of cylinder ordered
-     * @param montlyUsedCylinder the number of cylinders used in the current months
+     * @param montlyUsedCylinder      the number of cylinders used in the current
+     *                                months
      */
-    public DomesticCylinder(String cylinderId, String cylinderType, String bookingId, double basePrice, double weight, String subsidyAmount, String citizenshipNumber, int quantityOfOrderCylinder, int monthlyUsedCylinder ){
+    public DomesticCylinder(String cylinderId, String cylinderType, String bookingId, double basePrice, double weight,
+            String subsidyAmount, String citizenshipNumber, int quantityOfOrderCylinder, int monthlyUsedCylinder) {
         super(cylinderId, cylinderType, bookingId, basePrice, weight);
     }
 
-    //Getter methods
+    // Getter methods
 
     /**
      * Retruns the subsidy amount
      * 
      * @return the subsidy amount
      */
-    public double getSubsidyAmount(){
+    public double getSubsidyAmount() {
         return subsidyAmount;
     }
 
@@ -45,7 +49,7 @@ public class DomesticCylinder extends LPGCylinder {
      * 
      * @return the citizenship number of customer
      */
-    public String getCitizenshipNumber(){
+    public String getCitizenshipNumber() {
         return citizenshipNumber;
     }
 
@@ -54,7 +58,7 @@ public class DomesticCylinder extends LPGCylinder {
      * 
      * @return quantity of ordered cylinder
      */
-    public int getQuantityOfOrderedCylinder(){
+    public int getQuantityOfOrderedCylinder() {
         return quantityOfOrderCylinder;
     }
 
@@ -63,31 +67,59 @@ public class DomesticCylinder extends LPGCylinder {
      * 
      * @return the total cylinder used in a month
      */
-    public int getMonthlyUsedCylinder(){
+    public int getMonthlyUsedCylinder() {
         return monthlyUsedCylinder;
     }
 
-    //Setter method
-    
+    // Setter method
+
     /**
-     * Sets the subsidy amount for the domestic cyclinder
-     * Subsidy is only allowed if the customer has valid 
-     * citienship number and a monthly usage of no more than
-     * two cylinders.
+     * Sets the subsidy amount for the domestic cyclinder if 
+     * customer is eligible
      * 
      * @param subsidyAmount the subsidy amount to be asigned
      */
-    public void setSubsidyAmount(double subsidyAmount){
-        if(subsidyAmount <= 0){
-            System.out.println("Subsidy amoun must be greater than ");
+    public void setSubsidyAmount(double subsidyAmount) {
+        if (subsidyAmount < 0) {
+            System.out.println("Subsidy amount cannot be less than 0.");
+
+        } else if(!isEligibleForSubsidy()){
+            System.out.println("Customer is not eligble for subsidy.");
             return;
-        }
-        if(this.citizenshipNumber == null  || this.citizenshipNumber.trim().isEmpty() || this.monthlyUsedCylinder > 2 ){
-            System.out.println("Subsidy not allowed");
         } else {
             this.subsidyAmount = subsidyAmount;
         }
-
     }
-    
+
+    /**
+     * Sets citizenship number of the customer
+     * The citizenship number must not be null, empty,
+     * and must contian exactly 12 characters.
+     * 
+     * @param citizenshipNumber the citizenship number to be assigned
+     * 
+     */
+    public void setCitizenshipNumber(String citizenshipNumber) {
+        if (citizenshipNumber == null || citizenshipNumber.trim().isEmpty()) {
+            System.out.println("Citizenship cannit be empty!");
+        } else if (citizenshipNumber.length() != 12) {
+            System.out.println("Citizenship number must be exactly 12 characters.");
+        } else {
+            this.citizenshipNumber = citizenshipNumber.trim();
+
+        }
+    }
+    /**
+     * Checks whether the customer is eligible for a subsidy
+     * A customer is eligible if they have a valid citizenship number
+     * and have used no more than 2 cylinder in a month.
+     * 
+     * @return true if the customer is eligible for a subsidy, otherwise fasle
+     */
+    public boolean isEligibleForSubsidy() {
+        boolean validCitizenship = this.citizenshipNumber != null
+                && citizenshipNumber.trim().length() == 12;
+        boolean withInQuota = this.monthlyUsedCylinder <= 2;
+        return validCitizenship && withInQuota;
+    }
 }
