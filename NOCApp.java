4ba69540f7266af04ba4e8ef312116879df33e9c
Milan Rai <milanraii014@gmail.com>
2026-07-15 18:33:17 +0545

    push

--- NOCApp.java
+++ NOCApp.java
@@ -118,7 +118,7 @@ public class NOCApp extends JFrame {
         setTitle("LPG Cylinder Booking Management System");
         setSize(1200, 800);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);KO
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
         // Cylinder Type
